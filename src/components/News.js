7d9a422291c950b71a80d8b0d8e771db8ab395a0
Arnab <arnabdastriker@gmail.com>
2023-04-18 02:41:47 +0530

    Pagination

--- src/components/News.js
+++ src/components/News.js
@@ -9,17 +9,47 @@ export class News extends Component {
     this.state={
       articles : [],
       loading : false,
+      page :1,
+      totalResults : 0
     }
   }
   
   async componentDidMount(){
-    let url= "https://newsapi.org/v2/everything?q=tesla&from=2023-03-17&sortBy=publishedAt&apiKey=2b21f840ae484ddbaf065ee469d944f7"
+    let url= `https://newsapi.org/v2/top-headlines?country=in&apiKey=2b21f840ae484ddbaf065ee469d944f7&pageSize=6`
     let unparsedData = await fetch(url);
     let data = await unparsedData.json();
 
-    this.setState({articles: data.articles})
+    this.setState({
+      articles: data.articles,
+      totalResults : data.totalResults,
+    })
   }
   render() {
+
+
+    const handlePrevClick= async ()=>
+    {
+      let url= `https://newsapi.org/v2/top-headlines?country=in&apiKey=2b21f840ae484ddbaf065ee469d944f7&pageSize=6&pageSize=6&page=${this.state.page -1}`
+      let unparsedData = await fetch(url);
+      let data = await unparsedData.json();
+
+    this.setState({
+      articles: data.articles,
+      page : this.state.page -1,
+    })
+    }
+
+    const handleNextClick =async ()=>{
+      let url= `https://newsapi.org/v2/top-headlines?country=in&apiKey=2b21f840ae484ddbaf065ee469d944f7&pageSize=6&page=${this.state.page +1}`
+      let unparsedData = await fetch(url);
+      let data = await unparsedData.json();
+
+    this.setState({
+      articles: data.articles,
+      page : this.state.page +1,
+    })
+    }
+
     return (
       <div className="container my-3">
         <div className="row">
@@ -28,6 +58,10 @@ export class News extends Component {
                       <NewsItem title={element.title} description={element.description} imageUrl={element.urlToImage} url={element.url} />
                     </div>
           })}
+        </div>
+        <div className="container d-flex justify-content-between">
+          <button type="button" disabled={this.state.page<=1} onClick={handlePrevClick} className="btn btn-danger">Previous</button>
+          <button type="button" disabled={this.state.page>=this.state.totalResults/6}onClick={handleNextClick} className="btn btn-danger">Next</button>
         </div>
       </div>
     );

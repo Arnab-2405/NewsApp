2d37a7ec04215f6cf9687ee2a45ebad5f95bd6f1
Arnab <arnabdastriker@gmail.com>
2023-04-20 16:31:32 +0530

    Added Loading Wheel

--- src/components/News.js
+++ src/components/News.js
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
 import NewsItem from "./NewsItem";
+import Spinner from "./Spinner";
 
 export class News extends Component {
 
@@ -15,13 +16,15 @@ export class News extends Component {
   }
   
   async componentDidMount(){
-    let url= `https://newsapi.org/v2/top-headlines?country=in&apiKey=2b21f840ae484ddbaf065ee469d944f7&pageSize=6`
+    let url= `ttps://newsapi.org/v2/top-headlines?country=in&apiKey=2b21f840ae484ddbaf065ee469d944f7&pageSize=${this.props.pageSize}`
+    this.setState({loading : true})
     let unparsedData = await fetch(url);
     let data = await unparsedData.json();
 
     this.setState({
       articles: data.articles,
       totalResults : data.totalResults,
+      loading: false
     })
   }
   render() {
@@ -29,41 +32,49 @@ export class News extends Component {
 
     const handlePrevClick= async ()=>
     {
-      let url= `https://newsapi.org/v2/top-headlines?country=in&apiKey=2b21f840ae484ddbaf065ee469d944f7&pageSize=6&pageSize=6&page=${this.state.page -1}`
+      let url= `https://newsapi.org/v2/top-headlines?country=in&apiKey=2b21f840ae484ddbaf065ee469d944f7&pageSize=${this.props.pageSize}&page=${this.state.page -1}`
+      this.setState({loading : true})
       let unparsedData = await fetch(url);
       let data = await unparsedData.json();
 
     this.setState({
       articles: data.articles,
       page : this.state.page -1,
+      loading: false
     })
     }
 
     const handleNextClick =async ()=>{
-      let url= `https://newsapi.org/v2/top-headlines?country=in&apiKey=2b21f840ae484ddbaf065ee469d944f7&pageSize=6&page=${this.state.page +1}`
+      let url= `https://newsapi.org/v2/top-headlines?country=in&apiKey=2b21f840ae484ddbaf065ee469d944f7&pageSize=${this.props.pageSize}&page=${this.state.page +1}`
+      this.setState({loading : true})
       let unparsedData = await fetch(url);
       let data = await unparsedData.json();
 
     this.setState({
       articles: data.articles,
       page : this.state.page +1,
+      loading :false
     })
     }
 
     return (
+      <>
+      <h1 className="text-center">Top Headlines</h1>
+      {this.state.loading && <Spinner/>}
       <div className="container my-3">
-        <div className="row">
+        {!this.state.loading && <div className="row">
           {this.state.articles.map((element)=>{
             return <div className="col-md-4" key={element.url}>
                       <NewsItem title={element.title} description={element.description} imageUrl={element.urlToImage} url={element.url} />
                     </div>
           })}
-        </div>
+        </div>}
         <div className="container d-flex justify-content-between">
           <button type="button" disabled={this.state.page<=1} onClick={handlePrevClick} className="btn btn-danger">Previous</button>
-          <button type="button" disabled={this.state.page>=this.state.totalResults/6}onClick={handleNextClick} className="btn btn-danger">Next</button>
+          <button type="button" disabled={this.state.page>=Math.ceil(this.state.totalResults/this.props.pageSize)}onClick={handleNextClick} className="btn btn-danger">Next</button>
         </div>
       </div>
+      </>
     );
   }
 }
